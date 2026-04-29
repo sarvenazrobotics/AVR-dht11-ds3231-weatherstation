8edae0787a7b5379827c3ac2ca68e0955d815b74
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-04-29 12:32:04 +0200

    lcd modes and libraries

---
+++ src/lcd_i2c.c
@@ -0,0 +1,30 @@
+/*
+ * lcd_i2c.c
+ *
+ * Created: 4/29/2026 12:09:05 PM
+ * Author: Sarve
+ */
+
+#include <io.h>
+#include <delay.h>
+#include "lcd_i2c.h"
+
+
+#define LCD_ADDR 0x27
+
+#define LCD_CLEAR 0x01
+#define LCD_HOME 0x02
+#define LCD_ENTRY_MODE 0x06
+#define LCD_DISPLAY_ON 0x0C
+#define LCD_FUNCTION_4BIT 0x28
+#define LCD_LINE1 0x80
+#define LCD_LINE2 0xC0
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
